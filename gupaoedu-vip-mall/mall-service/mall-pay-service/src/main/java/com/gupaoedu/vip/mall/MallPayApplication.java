ebe4832b8ac108a2f7e920849df67fbd9021ee2d
caoliangqiang <clq152822>
2025-11-20 15:34:02 +0800

    商城提交

---
+++ gupaoedu-vip-mall/mall-service/mall-pay-service/src/main/java/com/gupaoedu/vip/mall/MallPayApplication.java
@@ -0,0 +1,31 @@
+package com.gupaoedu.vip.mall;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/*****
+ * @Author:
+ * @Description:
+ ****/
+@SpringBootApplication
+@MapperScan(basePackages = {"com.gupaoedu.vip.mall.pay.mapper"})
+public class MallPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallPayApplication.class,args);
+    }
+
+    /****
+     * 微信支付SDK对象
+     * @param weixinPayConfig
+     * @return
+     * @throws Exception
+     */
+//    @Bean
+//    public WXPay wxPay(WeixinPayConfig weixinPayConfig) throws Exception {
+//        return new WXPay(weixinPayConfig);
+//    }
+}
---
+++ gupaoedu-vip-mall/mall-service/mall-pay-service/src/main/java/com/gupaoedu/vip/mall/pay/mapper/PayLogMapper.java
@@ -0,0 +1,13 @@
+package com.gupaoedu.vip.mall.pay.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gupaoedu.vip.mall.pay.model.PayLog;
+import org.apache.ibatis.annotations.Mapper;
+
+/*****
+ * @Author:
+ * @Description:
+ ****/
+@Mapper
+public interface PayLogMapper extends BaseMapper<PayLog> {
+}
